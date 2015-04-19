436542a6239d0e9c425604732c427bf4522631ca
Filip Wieland <filiplamparski@gmail.com>
2015-04-19 14:12:02 +0100

    Load codemirror with browserify

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-unused-vars */
+/* global require, riot */
 
 import Loader from './components/loader.js';
 import Menu from './components/menu.js';
 import Keyboard from './controls/keyboard.js';
 import * as Models from './models.js';
+import { loadCSS, loadJS } from './utils.js';
+
+window.commonmark = require('commonmark');
+window.riot = require('riot');
+window.CodeMirror = require('codemirror');
+require('codemirror/mode/markdown/markdown');
 
 window.ANIM_END = (function (){ //eslint-disable-line no-unused-vars
   let t;
@@ -25,5 +32,17 @@ window.ANIM_END = (function (){ //eslint-disable-line no-unused-vars
 $(function() {
   Menu.init();
   Keyboard.init();
-  Loader.hide();
+  Promise.all([
+    loadCSS('assets/codemirror.css'),
+    loadCSS('assets/monokai.css'),
+    loadJS('templates/markdown.js'),
+    loadJS('templates/lesson_test.js'),
+    loadJS('templates/overview.js')])
+  .then(() => {
+    console.log('CodeMirror loaded');
+    setTimeout(function() {
+      riot.mount($('lesson-test').get(0), {});
+      Loader.hide();
+    }, 20);
+  });
 });
